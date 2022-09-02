9bcc1baba18c4d9e2c5f39dd0c797c16e91f1e1e
slav344 <petro.ivanov9901@gmail.com>
2022-09-02 14:48:45 +0300

    25lesson

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Назва: ', widget=forms.TextInput(attrs={"class": "form-control"}))
-    content = forms.CharField(label='Текст: ', required=False, widget=forms.Textarea(
-        attrs={"class": "form-control", "rows": 5}))
-    is_published = forms.BooleanField(label='Надруковано?: ', initial=True)
-    category = forms.ModelChoiceField(empty_label='Виберіть категорію', label='Категорія',
-                                      queryset=Category.objects.all(),widget=forms.Select(attrs={"class": "form-control"}))
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'})
+        }
