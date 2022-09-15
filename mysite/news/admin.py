594c0896be2bd288d24523ee23cd617e2156fb72
slav344 <petro.ivanov9901@gmail.com>
2022-09-15 17:04:08 +0300

    41 lesson

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return '_'
+
+    get_photo.short_description = 'Мініатюра'
 
 
 class CategoryAdmin(admin.ModelAdmin):
