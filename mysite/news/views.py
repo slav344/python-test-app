b6eae692d7642eedfdd630a9ce70a246b013ddbe
slav344 <petro.ivanov9901@gmail.com>
2022-09-19 21:28:15 +0300

    43 lesson

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,19 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .forms import NewsForm
 from .models import News, Category
 from .utils import MyMixin
+
+
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 class HomeNews(ListView, MyMixin):
